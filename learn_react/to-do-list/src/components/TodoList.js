307e0da083fc0dcf3f662a26b154f2b8d90e265f
Aditya Mukati <adityamukati8564@gmail.com>
2024-05-03 07:07:01 +0530

    ldw

--- learn_react/to-do-list/src/components/TodoList.js
+++ learn_react/to-do-list/src/components/TodoList.js
@@ -1,42 +1,56 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import '../styles.css'; // Import the CSS file
 
-function TodoList() {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      newTask: ''
+    };
+  }
 
-  const addTask = () => {
+  // Function to add a new task
+  addTask = () => {
+    const { tasks, newTask } = this.state;
     if (newTask.trim() !== '') {
-      setTasks([...tasks, newTask]);
-      setNewTask('');
+      this.setState({
+        tasks: [...tasks, newTask],
+        newTask: ''
+      });
     }
   };
 
-  const deleteTask = (index) => {
+  // Function to delete a task
+  deleteTask = (index) => {
+    const { tasks } = this.state;
     const updatedTasks = tasks.filter((task, i) => i !== index);
-    setTasks(updatedTasks);
+    this.setState({ tasks: updatedTasks });
   };
 
-  return (
-    <div className="container">
-      <h1>To-Do List</h1>
-      <input
-        type="text"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-        placeholder="Add a new task"
-      />
-      <button onClick={addTask}>Add Task</button>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
-            <button onClick={() => deleteTask(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  render() {
+    const { tasks, newTask } = this.state;
+    return (
+      <div className="container">
+        <h1>To-Do List</h1>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => this.setState({ newTask: e.target.value })}
+          placeholder="Add a new task"
+        />
+        <button onClick={this.addTask}>Add Task</button>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <button onClick={() => this.deleteTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default TodoList;
